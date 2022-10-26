5cb4005766369364d1a21f38580839998c5b8278
LALITH0110 <lalith.kothuru@gmail.com>
2022-10-25 23:52:01 -0500

    Updated Student

--- ExamPracticeProblems2/Student.java
+++ ExamPracticeProblems2/Student.java
@@ -1,65 +1,69 @@
-
 public class Student {
-	public static final char DEFAULT_GENDER= 'N';
-	public static final String DEFAULT_LENGTH= "***";
-	public char gender;
-	private String length;
-	
-	public Student() {
-		gender = DEFAULT_GENDER;
-		length = DEFAULT_LENGTH;
-		
-	}
-	public Student(char genderO , String newLength) {
-		gender = genderO;
-		length = newLength;
-		if (genderO == 'M' || genderO == 'F' || genderO == 'N') {
-			gender = genderO;
-		} else {
-			gender = DEFAULT_GENDER;
-		}
-		if (newLength == null) {
-			length = DEFAULT_LENGTH;
-		} else {
-			length = newLength;
-		}
-	
-	}
-	
-     public String setRoom(String room) {
-    	char secondChar = room.charAt(1);
-    	char thirdChar = room.charAt(2);
-    	if (secondChar >= '2' && secondChar <='9' ) {
-    		if (secondChar >='2' && secondChar <= '4') {
-    			if (thirdChar >= '1' && thirdChar <='6') {
-    				length = room;
-    			} else {
-    				length = DEFAULT_LENGTH;
-    			}
-    		} else if ( secondChar > '4' && secondChar <= '7') {
-    			if (thirdChar >= '1' && thirdChar <='3') {
-    				length = room;
-    			} else {
-    				length = DEFAULT_LENGTH;
-    			}
-    		}else {
-    			if (thirdChar == '1' && thirdChar == '2') {
-    				length = room;
-    			} else {
-    				length = DEFAULT_LENGTH;
-    			}
-    		}
-    	}
-    	else {
-    		length = DEFAULT_LENGTH;
-    	}
-		return length;
-	}
 
-	public String toString() {
-		return "Gender:" +gender + " " + "Room:" + length;
-		}
-	
-	
+    private static final char DEFAULT_GENDER = 'N';
+    private static final String DEFAULT_LENGTH = "***";
+    private char gender;
+    private String length;
 
+    public Student(){
+        gender = DEFAULT_GENDER;
+        length = DEFAULT_LENGTH;
+    }
+    public Student(char genderO , String newLength){
+        if (genderO == 'M' || genderO == 'F' || genderO == 'N'){
+            gender = genderO;
+        }
+        else {
+            gender = DEFAULT_GENDER;
+        }
+        if (newLength== null){
+            length = DEFAULT_LENGTH;
+        }
+        else {
+            length = newLength;
+        }
+
+    }
+
+    public String setRoom(String room){
+        char secondChar = room.charAt(1);
+        char thirdChar = room.charAt(2);
+        if (secondChar >= 50 && secondChar <= 57){
+            if (secondChar>= 50 && secondChar <= 52 ) {
+                if (thirdChar >= 49 && thirdChar <= 54) {
+                    length = room;
+                }
+                else {
+                    length = DEFAULT_LENGTH;
+                }
+            }
+            else if (secondChar>= 50 && secondChar<= 55){
+                if (thirdChar>= 49 && thirdChar<=51){
+                    length = room;
+                }
+                else {
+                    length = DEFAULT_LENGTH;
+                }
+            }
+            else if (secondChar>= 50 && secondChar <= 57 ) {
+                if (thirdChar >= 49 && thirdChar <=50){
+                    length = room;
+                }
+                else {
+                    length = DEFAULT_LENGTH;
+                }
+            }
+            else {
+                length = DEFAULT_LENGTH;
+            }
+          }
+        else {
+            length= DEFAULT_LENGTH;
+        }
+        return length;
+
+    }
+    public String toString(){
+        return "Gender:"+gender+" Room:"+length;
+    }
 }
